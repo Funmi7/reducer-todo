dd623f555f361d142f93ffebd16db022770f866c
Funmi7 <talabifunmilayo@gmail.com>
2019-10-08 13:52:35 +0100

    Rendered the initial todo list in my app

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+       return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.item}
     </div>
   );
 }
